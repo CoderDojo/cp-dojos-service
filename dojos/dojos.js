57bbff1cb6e50d6d6951e455227c6a37cf9228fa
sigismondm <sigismond.mclaughlin@nearform.com>
2015-03-19 21:33:09 -0500

    Initial commit

---
+++ dojos/dojos.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function (options) {
+  var seneca = this;
+  var plugin = 'cd-dojos';
+  var version = '1.0';
+  var ENTITY_NS = "cd/dojos";
+
+  seneca.add({ role: plugin, cmd: 'search'}, cmd_search);
+  seneca.add({ role: plugin, cmd: 'create'}, cmd_create);
+  seneca.add({ role: plugin, cmd: 'delete'}, cmd_delete);
+
+  function cmd_search(args, done){
+    var seneca = this, query = {}, dojos_ent;
+    query = args.query;
+
+    dojos_ent = seneca.make$(ENTITY_NS);
+    dojos_ent.list$(query, done);
+  }
+
+  function cmd_create(args, done){
+    var seneca = this, query = {}, dojo_ent, dojo;
+    dojo = args.dojo;
+
+    seneca.make$(ENTITY_NS).save$(dojo, function(err, response) {
+      if(err){
+        return done(err);
+      }
+
+      done(null, response);
+    });
+  }
+
+  function cmd_delete(args, done){
+    var seneca = this;
+    var id = args.id;
+
+    seneca.make$(ENTITY_NS).remove$( id, function(err){
+      if(err){
+        return done(err);
+      }
+      done();
+    });
+  }
+
+   // web interface
+  seneca.act({ role: 'web', use: {
+    prefix: '/',
+    pin: { role: plugin, cmd: '*' },
+    map: {
+      'search': {POST: true, alias: 'dojos/search'},
+      'create': {POST: true, alias: 'dojos'},
+      'delete': {DELETE: true, alias: 'dojos/:id'}
+    }
+  }});
+
+
+  return {
+    name: plugin
+  };
+
+}
---
+++ dojos/test/dojos-spec.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var seneca = require('seneca')(),
+    express = require('express'),
+    request = require('supertest');
+
+var bodyparser   = require('body-parser')
+
+var app = require('express')()
+
+seneca.use( require('seneca-web') );
+seneca.use('../dojos.js');
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json({ limit: 100000}));
+app.use(seneca.export('web'));
+
+
+seneca.use('mongo-store',{
+  name:'zen_live',
+  host:'127.0.0.1',
+  port:27018,
+})
+
+var testDojo = {
+  "id": 9999999,
+  "name": "Test",
+  "creator": 999999,
+  "created": "2015-03-12T02:49:49.000Z",
+  "verified_at": null,
+  "verified_by": null,
+  "verified": 0,
+  "need_mentors": 0,
+  "stage": 0,
+  "time": "Thursday, once a month, 6-8pm",
+  "country": "US",
+  "location": "Test Public Library",
+  "coordinates": "40.4917889,-74.4453375",
+  "notes": "<p>None</p>",
+  "email": null,
+  "website": "http://test.com",
+  "twitter": null,
+  "google_group": null,
+  "eb_id": null,
+  "supporter_image": null,
+  "deleted": 0,
+  "deleted_by": null,
+  "deleted_at": null,
+  "private": 0,
+  "url_slug": null,
+  "continent": "NA",
+  "alpha2": "US",
+  "alpha3": "USA",
+  "number": 840,
+  "country_name": "United States of America"
+}
+
+var testResponse = [ { 
+  'entity$': '-/cd/dojos',
+  name: 'Test',
+  creator: 999999,
+  created: '2015-03-12T02:49:49.000Z',
+  verified_at: null,
+  verified_by: null,
+  verified: 0,
+  need_mentors: 0,
+  stage: 0,
+  time: 'Thursday, once a month, 6-8pm',
+  country: 'US',
+  location: 'Test Public Library',
+  coordinates: '40.4917889,-74.4453375',
+  notes: '<p>None</p>',
+  email: null,
+  website: 'http://test.com',
+  twitter: null,
+  google_group: null,
+  eb_id: null,
+  supporter_image: null,
+  deleted: 0,
+  deleted_by: null,
+  deleted_at: null,
+  private: 0,
+  url_slug: null,
+  continent: 'NA',
+  alpha2: 'US',
+  alpha3: 'USA',
+  number: 840,
+  country_name: 'United States of America',
+  id: '9999999' }];
+
+
+describe('Dojo Microservice test', function(){
+
+  before(function(done){
+    seneca.ready(function(){
+      request(app)
+        .post('/dojos')
+        .send({dojo: testDojo})
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+    });
+  })
+
+  describe('POST /dojos/search', function(){
+    it('respond with json', function(done){      
+      seneca.ready(function(){
+        request(app)
+          .post('/dojos/search')
+          .send({query:{_id: testDojo.id}})
+          .expect(200, testResponse ,done);
+      });
+    });
+  });
+
+  after(function(done){
+    seneca.ready(function(){
+      request(app)
+        .delete('/dojos/' + testDojo.id)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+
+});
+
+
+
+
+
