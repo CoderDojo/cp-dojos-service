9bc2c3191bbcb40f1756fb239c3242289130e840
sigismondm <sigismond.mclaughlin@nearform.com>
2015-04-09 10:00:21 -0500

    Update fixtures and tests

--- dojos/test/dojos-spec.js
+++ dojos/test/dojos-spec.js
@@ -15,46 +15,52 @@ console.log('using configuration', JSON.stringify(config, null, 4));
 seneca.options(config);
 
 seneca
-  .use('mongo-store', config["mongo-store"])
+  .use('postgresql-store', config["postgresql-store"])
   .use('../../dojos.js');
 
 
 describe('Dojo Microservice test', function(){
-  var usersEnt, dojosEnt;
+  var usersEnt, dojosEnt, usersDojosEnt;
 
   usersEnt = seneca.make$("sys/user");
   dojosEnt = seneca.make$("cd/dojos");
+  usersDojosEnt = seneca.make$("cd/usersdojos");
 
   //Empty Tables
   before(function(done){
     seneca.ready(function(){
-      dojosEnt.native$(function(err, db){
-        var collection = db.collection('cd_dojos');
-        collection.remove({}, function(err, noRemoved){
-          if(err){
-            return done(err);
-          } else {
-            return done();
-          }
-        });
+      dojosEnt.remove$({all$: 1}, function(err){
+        if(err){
+          return done(err);
+        }
+        done();
       });
     });
   });
 
   before(function(done){
     seneca.ready(function(){
-      usersEnt.native$(function(err, db){
-        var collection = db.collection('sys_users');
-        collection.remove({}, function(err, noRemoved){
-          if(err){
-            return done(err);
-          } else {
-            return done();
-          }
-        });
+      usersEnt.remove$({all$: 1}, function(err){
+        if(err){
+          return done(err);
+        }
+
+        done();
       });
     });
   });
+
+  before(function(done){
+    seneca.ready(function(){
+      usersDojosEnt.remove$({all$: 1}, function(err){
+        if(err){
+          return done(err);
+        }
+
+        return done();
+      })
+    })
+  })
 
 
   var loadUsers = function(user, cb){
@@ -74,7 +80,7 @@ describe('Dojo Microservice test', function(){
   });
 
 
-  describe('Save successfully', function(){
+  describe('Save', function(){
     it('respond with json', function(done){      
       seneca.ready(function(){
         seneca.act({role: role, cmd: 'create', dojo:  dojos[0], user: users[0].id}, 
@@ -82,12 +88,12 @@ describe('Dojo Microservice test', function(){
             if(err){
               return done(err);
             } else {
-              seneca.act({role: role, cmd: 'search', query: {_id: savedDojo.id}}, function(err, users){
+              seneca.act({role: role, cmd: 'search', query: {id: savedDojo.id}}, function(err, dojos){
                 if(err){
                   return done(err);
                 }
-                expect(users.length).to.be.equal(1);
-                expect(users[0]).to.be.ok;
+                expect(dojos.length).to.be.equal(1);
+                expect(dojos[0]).to.be.ok;
                 
                 done();
               });
@@ -96,5 +102,84 @@ describe('Dojo Microservice test', function(){
       });
     });
   });
+
+  describe('Listing', function(){
+    it('Should respond with json', function(done){
+      seneca.ready(function(){
+        seneca.act({role: role, cmd: 'list'}, function(err, dojos){
+          if(err){
+            return done(err);
+          } else {
+            expect(dojos.length).to.be.equal(1);
+            expect(dojos[0]).to.be.ok;
+
+            done();
+          }
+        });
+      });
+    });
+  });
+
+  before(function(done){
+    seneca.ready(function(){
+      seneca.act({role: role, cmd: 'create', dojo: dojos[1], user: users[1].id},
+        function(err, dojo){
+          if(err){
+            return done(err);
+          }
+          done();
+        });
+    });
+  });
+
+  describe('List all my dojos', function(){
+    it('Should list all my dojos', function(done){
+      seneca.ready(function(){
+        seneca.act({role: role, cmd: 'my_dojos_search', user: users[1], query: {skip: 0, limit: 10}}, function(err, dojos){
+          if(err){
+            return done(err);
+          }
+          expect(dojos.length).to.be.equal(1);
+          expect(dojos[0]).to.be.ok;
+
+          done();
+        })
+      });
+    })
+  });
+
+  describe('Count all my dojos', function(){
+    it('Should count all my dojos', function(done){
+      seneca.ready(function(){
+        seneca.act({role: role, cmd: 'my_dojos_count', user: users[1]}, function(err, noOfDojos){
+          if(err){
+            return done(err);
+          }
+          expect(noOfDojos).to.be.equal(1);
+          done();
+        })
+      });
+    })
+  });
+
+  describe('Update', function(){
+    it('Should return json', function(done){
+      var dojo = dojos[0];
+
+      dojo.notes = "updated";
+
+      seneca.ready(function(){
+        seneca.act({role: role, cmd: 'update', dojo: dojo}, function(err, updatedDojo){
+          if(err){
+            return done(err);
+          }
+
+          expect(updatedDojo.notes).to.be.equal("updated");
+          done();
+        });
+      })
+    })
+  });
+
 
 });
