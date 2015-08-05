4024d060079ef12b179733fee4953b7f141ac2ef
Damian Beresford <dberesford@gmail.com>
2015-08-05 22:47:45 +0100

    Stop app before starting in start.sh

--- deploy/start-server.sh
+++ deploy/start-server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+isExistApp=`ps -eaf |grep cp-dojos-service |grep -v grep| awk '{ print $2; }'`
+if [[ -n $isExistApp ]]; then
+    service cp-dojos-service stop
+fi
+
 service cp-dojos-service start
