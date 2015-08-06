81d3132ddd79fc79cb213a62776c09475fa5a1b0
david-cahill <davidrichardcahill@gmail.com>
2015-08-06 13:56:58 +0100

    allow cdf admins to edit dojos

--- dojos.js
+++ dojos.js
@@ -665,7 +665,6 @@ module.exports = function (options) {
 
   function cmd_update(args, done){
     var dojo = args.dojo;
-
     //load dojo before saving to get it's current state
     var dojoEnt = seneca.make$(ENTITY_NS);
     var dojoLeadsEnt = seneca.make$(DOJO_LEADS_ENTITY_NS);
@@ -688,8 +687,7 @@ module.exports = function (options) {
           dojo.verifiedAt = new Date();
           dojo.verifiedBy = args.user.id;
 
-          // need to deal with better, but stops the system from crashing for now.  
-          if(!dojo.dojoLeadId) return;
+          if(!dojo.dojoLeadId) return done(null, dojo);
 
           dojoLeadsEnt.load$(dojo.dojoLeadId, function(err, dojoLead) {
             if (err) {
