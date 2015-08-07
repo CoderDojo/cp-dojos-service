25ed6395e6cdebbdcd543b98991319694dc71f48
Damian Beresford <dberesford@gmail.com>
2015-08-07 10:39:35 +0100

    Move migrations into service.js

---
+++ migrate-psql-db.js
@@ -0,0 +1,20 @@
+var postgrator = require('postgrator');
+var config = require('./config/config.js')();
+
+module.exports = function migrate (cb) {
+
+  postgrator.setConfig({
+    migrationDirectory: './scripts/database/pg/migrations',
+    driver: 'pg',
+    host: config['postgresql-store'].host,
+    database: config['postgresql-store'].name,
+    username: config['postgresql-store'].username,
+    password: config['postgresql-store'].password
+  });
+
+  postgrator.migrate('max', function (err, migrations) {
+    postgrator.endConnection(function () {
+      return cb(err, migrations);
+    });
+  });
+}
--- start.sh
+++ start.sh
@@ -5,8 +5,4 @@ USAGE="Usage: ./$SCRIPT <config> <startscript> [startscript_opts]..."
 
 source "$PROJECT_DIR/scripts/exec_on_env.sh"
 
-if [ -z $DONOTMIGRATE ] ; then
-    run_js "$PROJECT_DIR/scripts/migrate-psql-db.js" || exit 1;
-fi 
-
 exec_js $@
