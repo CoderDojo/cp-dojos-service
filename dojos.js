52fc5c000e14b7d78dc11b784907c83afd10578b
Glen Keane <glenkeane.94@gmail.com>
2015-08-19 11:25:23 +0100

    add fatal flag to reverse gecoding commands

--- dojos.js
+++ dojos.js
@@ -622,7 +622,7 @@ module.exports = function (options) {
     async.waterfall([
       function (cb){
         if(dojo.geoPoint){
-          seneca.act({role: 'cd-countries', cmd: 'reverse_geocode', coords: dojo.geoPoint}, function(err, res){
+          seneca.act({role: 'cd-countries', cmd: 'reverse_geocode', coords: dojo.geoPoint, fatal$:false}, function(err, res){
             if(err){
               console.log(err);
               return cb();
@@ -722,7 +722,7 @@ module.exports = function (options) {
               lon: pair[1]
             }
           }
-          seneca.act({role: 'cd-countries', cmd: 'reverse_geocode', coords: dojo.geoPoint}, function(err, res){
+          seneca.act({role: 'cd-countries', cmd: 'reverse_geocode', coords: dojo.geoPoint, fatal$:false}, function(err, res){
             if(err){
               console.error(err);
               return updateLogic();
