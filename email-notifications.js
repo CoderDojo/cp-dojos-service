5944e83b13e03d2d41deb52dfeb2c43e20515378
david-cahill <davidrichardcahill@gmail.com>
2015-08-28 09:24:18 +0100

    add console statement to debug email error

--- email-notifications.js
+++ email-notifications.js
@@ -20,6 +20,7 @@ module.exports = function( options ) {
   function send_notification(args, done) {
     if (options.sendemail && options.email) {
       var email = options.email[args.code];
+      console.log('*** args.code = ' + args.code);
       if (!fs.existsSync(path.join(__dirname, '/email-templates/', args.code))) {
         args.code = args.code.substring(0, args.code.length-5) + 'en_US';
       }
