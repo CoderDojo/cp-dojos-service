40d99f85c68e7986c970d2a8ff8562fd101b333c
Wardormeur <wardormeur@localhost.localdomain>
2016-04-12 09:38:22 +0100

    Filter user having no-emails or send it to their parents

--- dojos.js
+++ dojos.js
@@ -2123,6 +2123,7 @@ module.exports = function (options) {
   }
 
   function cmd_notify_all_members (args, done) {
+    //  TODO: enqueue this process
     var seneca = this;
     var dojoId = args.data.dojoId;
     var eventId = args.data.eventId;
@@ -2131,6 +2132,7 @@ module.exports = function (options) {
 
     async.waterfall([
       getDojoUsers,
+      checkEmail,
       getEvent,
       getDojo,
       sendEmails
@@ -2140,6 +2142,30 @@ module.exports = function (options) {
       var query = {dojoId: dojoId};
 
       seneca.act({role: plugin, cmd: 'load_dojo_users', query: query}, done);
+    }
+
+    function checkEmail (users, done) {
+      async.map(users, function (user, callback) {
+        if (_.isEmpty(user.email)) {
+          seneca.act({role: 'cd-profiles', cmd: 'load_parents_for_user', userId: user.id}, function (err, parents) {
+            if (err) return seneca.log.warn('No parent found for', user.id);
+            //  TODO: handle multiple parents
+            user.parent = parents[0];
+
+            //  excluse this child if the parent email is already in the list, to avoid multiple emails
+            if (_.some(users, {email: user.parent.email})) {
+              user = void 0;
+            }
+            callback(null, user);
+          });
+        } else {
+          callback(null, user);
+        }
+      },
+      function (err, users) {
+        if (err) done(err);
+        done(null, _.pull(users, void 0));
+      });
     }
 
     function getEvent (users, done) {
@@ -2169,10 +2195,11 @@ module.exports = function (options) {
         };
 
         var code = '';
+        var baseCode = '';
         if (event.type === 'recurring') {
-          code = 'notify-all-members-recurring-';
+          baseCode = 'notify-all-members-recurring-';
         } else {
-          code = 'notify-all-members-oneoff-';
+          baseCode = 'notify-all-members-oneoff-';
           var startDateUtcOffset = moment(_.head(event.dates).startTime).utcOffset();
           var endDateUtcOffset = moment(_.head(event.dates).endTime).utcOffset();
 
@@ -2185,11 +2212,22 @@ module.exports = function (options) {
         }
         var locality = args.locality || 'en_US';
         emailSubject = emailSubject + ' ' + dojo.name;
+
         _.forEach(users, function (user) {
           content.dojoMember = user.name;
-          var payload = {replyTo: dojo.email, from: dojo.name + ' <' + dojo.email + '>', to: user.email,
-                        code: code, locality: locality, content: content, subject: emailSubject};
-          seneca.act({role: plugin, cmd: 'send_email', payload: _.cloneDeep(payload)});
+          var email = user.email;
+          code = baseCode;
+          if (!_.isEmpty(user.parent) && !_.isEmpty(user.parent.email)) {
+            email = user.parent.email;
+            code = 'parents-' + baseCode;
+            content.childrenName = user.name;
+            content.dojoMember = user.parent.name;
+          }
+          if (!_.isEmpty(email)) {
+            var payload = {replyTo: dojo.email, from: dojo.name + ' <' + dojo.email + '>', to: email,
+              code: code, locality: locality, content: content, subject: emailSubject};
+            seneca.act({role: plugin, cmd: 'send_email', payload: _.cloneDeep(payload)});
+          }
         });
         done();
       }
