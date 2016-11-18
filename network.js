57266a20854ff32f048e2c390c2f45c08ac88e66
Wardormeur <wardormeur@gmail.com>
2016-11-18 18:37:14 +0000

    unify network to oebject notation

--- network.js
+++ network.js
@@ -2,10 +2,10 @@
 
 module.exports = function (seneca) {
   seneca.listen()
-    .client({type: 'web', port: 10303, pin: 'role:cd-users,cmd:*'})
-    .client({type: 'web', port: 10303, pin: 'role:cd-agreements,cmd:*'})
-    .client({type: 'web', port: 10303, pin: 'role:cd-profiles,cmd:*'})
-    .client({type: 'web', port: 10303, pin: 'role:cd-user-profile,cmd:*'})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-agreements', cmd: '*'}})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}})
+    .client({type: 'web', port: 10303, pin: {role: 'cd-user-profile', cmd: '*'}})
     .client({type: 'web', port: 10304, pin: {role: 'cd-salesforce', cmd: '*'}})
-    .client({type: 'web', port: 10306, pin: 'role:cd-events,cmd:*'});
+    .client({type: 'web', port: 10306, pin: {role: 'cd-events', cmd: '*'}});
 };
