bbad7e30e936ed3e48682e085455519a86a59395
Cian Butler <butlerx@notthe.cloud>
2017-07-05 10:50:54 +0100

    update dockerfile (#336)
    * update dockerfile

--- network.js
+++ network.js
@@ -2,9 +2,49 @@
 
 module.exports = function (seneca) {
   seneca.listen()
-    .client({type: 'web', port: 10303, pin: {role: 'cd-users', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-agreements', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-profiles', cmd: '*'}})
-    .client({type: 'web', port: 10303, pin: {role: 'cd-user-profile', cmd: '*'}})
-    .client({type: 'web', port: 10306, pin: {role: 'cd-events', cmd: '*'}});
+    .client({
+      type: 'web',
+      host: process.env.CD_USERS || 'localhost',
+      port: 10303,
+      pin: {
+        role: 'cd-users',
+        cmd: '*'
+      }
+    })
+    .client({
+      type: 'web',
+      host: process.env.CD_USERS || 'localhost',
+      port: 10303,
+      pin: {
+        role: 'cd-agreements',
+        cmd: '*'
+      }
+    })
+    .client({
+      type: 'web',
+      host: process.env.CD_USERS || 'localhost',
+      port: 10303,
+      pin: {
+        role: 'cd-profiles',
+        cmd: '*'
+      }
+    })
+    .client({
+      type: 'web',
+      host: process.env.CD_USERS || 'localhost',
+      port: 10303,
+      pin: {
+        role: 'cd-user-profiles',
+        cmd: '*'
+      }
+    })
+    .client({
+      type: 'web',
+      host: process.env.CD_EVENTS || 'localhost',
+      port: 10306,
+      pin: {
+        role: 'cd-events',
+        cmd: '*'
+      }
+    });
 };
