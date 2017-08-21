ef4084303df9be5d4cb5cc3d004d53d8e5315a4b
butlerx <butlerx@notthe.cloud>
2017-08-21 18:03:37 +0100

    fix patch

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -5,26 +5,23 @@ set -e
 if [ "$CIRCLE_BRANCH" = "master" ]; then
   DEP_VER=latest
   HOST=$PROD_HOST
-  CA_CERT=$PROD_CA_CERT
-  ADMIN_CERT=$PROD_ADMIN_CERT
-  ADMIN_KEY=$PROD_ADMIN_CERT
+  echo "$PROD_CA_CERT" > ca.pem
+  echo "$PROD_ADMIN_KEY" > admin-key.pem
+  echo "$PROD_ADMIN_CERT" > admin.pem
 elif [ "$CIRCLE_BRANCH" = "staging" ]; then
   DEP_VER=staging
   HOST=$STAGING_HOST
-  CA_CERT=$STAGING_CA_CERT
-  ADMIN_CERT=$STAGING_ADMIN_CERT
-  ADMIN_KEY=$STAGING_ADMIN_CERT
+  echo "$STAGING_CA_CERT" > ca.pem
+  echo "$STAGING_ADMIN_KEY" > admin-key.pem
+  echo "$STAGING_ADMIN_CERT" > admin.pem
 else
   exit 0
 fi
-sudo apt-get update
-sudo apt-get install --only-upgrade docker -y
 docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-dojos-service:"$CIRCLE_SHA1" .
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
 docker push coderdojo/cp-dojos-service:"$CIRCLE_SHA1"
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl config set-cluster default-cluster --server=https://"${HOST}" --certificate-authority="${CA_CERT}"
-kubectl config set-credentials default-admin --certificate-authority="${CA_CERT}" --client-key="${ADMIN_KEY}" --client-certificate="${ADMIN_CERT}"
+kubectl config set-cluster default-cluster --server=https://api."$HOST" --certificate-authority-data=ca.pem
+kubectl config set-credentials default-admin --certificate-authority=ca.pem --client-key=admin-key.pem --client-certificate=admin.pem
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 kubectl config use-context default-system
-kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$CIRCLE_BUILD_SHA1"'"}]}}}}'
+kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$CIRCLE_SHA1"'"}]}}}}'
