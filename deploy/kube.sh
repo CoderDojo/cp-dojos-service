f580b28345404f89f05f264568d65b6323018aaa
butlerx <butlerx@notthe.cloud>
2017-08-22 10:15:11 +0100

    changes based on review

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -17,11 +17,11 @@ elif [ "$CIRCLE_BRANCH" = "staging" ]; then
 else
   exit 0
 fi
-docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-dojos-service:"$CIRCLE_SHA1" .
+docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-dojos-service:"$GIT_SHA1" .
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
-docker push coderdojo/cp-dojos-service:"$CIRCLE_SHA1"
-kubectl config set-cluster default-cluster --server=https://api."$HOST" --certificate-authority-data=ca.pem
+docker push coderdojo/cp-dojos-service:"$GIT_SHA1"
+kubectl config set-cluster default-cluster --server=https://api."$HOST" --certificate-authority=ca.pem
 kubectl config set-credentials default-admin --certificate-authority=ca.pem --client-key=admin-key.pem --client-certificate=admin.pem
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 kubectl config use-context default-system
-kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$GIT_SHA1"'"}]}}}}'
