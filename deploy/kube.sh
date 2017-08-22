fbde52152232dceab48ec60b2dbc08894fa87ac9
butlerx <butlerx@notthe.cloud>
2017-08-22 16:59:54 +0100

    add base64 decode

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -5,39 +5,23 @@ set -e
 if [ "$CIRCLE_BRANCH" = "master" ]; then
   DEP_VER=latest
   HOST=$PROD_HOST
-  CA_CERT=$PROD_CA_CERT
-  ADMIN_CERT=$PROD_ADMIN_CERT
-  ADMIN_KEY=$PROD_ADMIN_KEY
+  echo "$PROD_CA_CERT" | base64 --decode > ca.pem
+  echo "$PROD_ADMIN_KEY" | base64 --decode > admin-key.pem
+  echo "$PROD_ADMIN_CERT" | base64 --decode > admin.pem
 elif [ "$CIRCLE_BRANCH" = "staging" ]; then
   DEP_VER=staging
   HOST=$STAGING_HOST
-  CA_CERT=$STAGING_CA_CERT
-  ADMIN_CERT=$STAGING_ADMIN_CERT
-  ADMIN_KEY=$STAGING_ADMIN_KEY
+  echo "$STAGING_CA_CERT" | base64 --decode > ca.pem
+  echo "$STAGING_ADMIN_KEY" | base64 --decode > admin-key.pem
+  echo "$STAGING_ADMIN_CERT" | base64 --decode > admin.pem
 else
   exit 0
 fi
 docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-dojos-service:"$GIT_SHA1" .
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
 docker push coderdojo/cp-dojos-service:"$GIT_SHA1"
-mkdir -p ~/.kube
-echo "apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: $CA_CERT
-    server: https://api.$HOST
-  name: default-cluster
-contexts:
-- context:
-    cluster: default-cluster
-    user: default-admin
-  name: default-system
-current-context: default-system
-kind: Config
-preferences: {}
-users:
-- name: default-admin
-  user:
-    client-certificate-data: $ADMIN_CERT
-    client-key-data: $ADMIN_KEY" > ~/.kube/config
+kubectl config set-cluster default-cluster --server=https://api."$HOST" --certificate-authority=ca.pem
+kubectl config set-credentials default-admin --certificate-authority=ca.pem --client-key=admin-key.pem --client-certificate=admin.pem
+kubectl config set-context default-system --cluster=default-cluster --user=default-admin
+kubectl config use-context default-system
 kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$GIT_SHA1"'"}]}}}}'
