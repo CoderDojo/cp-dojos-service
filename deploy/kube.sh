ae8d9ac02e8b1b29b5c22a045a9b571f4b552244
butlerx <butlerx@notthe.cloud>
2017-08-22 14:58:50 +0100

    echo kube config

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -5,23 +5,39 @@ set -e
 if [ "$CIRCLE_BRANCH" = "master" ]; then
   DEP_VER=latest
   HOST=$PROD_HOST
-  echo "$PROD_CA_CERT" > ca.pem
-  echo "$PROD_ADMIN_KEY" > admin-key.pem
-  echo "$PROD_ADMIN_CERT" > admin.pem
+  CA_CERT=$PROD_CA_CERT
+  ADMIN_CERT=$PROD_ADMIN_CERT
+  ADMIN_KEY=$PROD_ADMIN_KEY
 elif [ "$CIRCLE_BRANCH" = "staging" ]; then
   DEP_VER=staging
   HOST=$STAGING_HOST
-  echo "$STAGING_CA_CERT" > ca.pem
-  echo "$STAGING_ADMIN_KEY" > admin-key.pem
-  echo "$STAGING_ADMIN_CERT" > admin.pem
+  CA_CERT=$STAGING_CA_CERT
+  ADMIN_CERT=$STAGING_ADMIN_CERT
+  ADMIN_KEY=$STAGING_ADMIN_KEY
 else
   exit 0
 fi
 docker build --rm=false --build-arg DEP_VERSION=$DEP_VER -t coderdojo/cp-dojos-service:"$GIT_SHA1" .
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
 docker push coderdojo/cp-dojos-service:"$GIT_SHA1"
-kubectl config set-cluster default-cluster --server=https://api."$HOST" --certificate-authority=ca.pem
-kubectl config set-credentials default-admin --certificate-authority=ca.pem --client-key=admin-key.pem --client-certificate=admin.pem
-kubectl config set-context default-system --cluster=default-cluster --user=default-admin
-kubectl config use-context default-system
+mkdir -p ~/.kube
+echo "apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: $CA_CERT
+    server: https://api.$HOST
+  name: default-cluster
+contexts:
+- context:
+    cluster: default-cluster
+    user: default-admin
+  name: default-system
+current-context: default-system
+kind: Config
+preferences: {}
+users:
+- name: default-admin
+  user:
+    client-certificate-data: $ADMIN_CERT
+    client-key-data: $ADMIN_KEY" > ~/.kube/config
 kubectl patch deployment dojos -p '{"spec":{"template":{"spec":{"containers":[{"name":"dojos","image":"coderdojo/cp-dojos-service:'"$GIT_SHA1"'"}]}}}}'
