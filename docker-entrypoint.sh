73b5a068494bfb2a7b88ffc380aba499f6a87c28
butlerx <butlerx@notthe.cloud>
2017-08-25 09:23:06 +0100

    only run npm install on first setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
-cd /usr/src/cp-translations || exit
-npm link
 cd /usr/src/app || exit
-npm install && npm link cp-translations
+if [ ! -d "node_modules" ]; then
+  npm install
+  rm -rf node_modules/cp-translations
+  ln -s /usr/src/cp-translations node_modules/cp-translations
+fi
 nodemon service.js
