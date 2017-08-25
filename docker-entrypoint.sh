1168f7b0c0db97ac54fa1d10b65dbe25d1a529d4
butlerx <butlerx@notthe.cloud>
2017-08-25 14:23:39 +0100

    check if the package file has changed rather then if a node muodules exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env sh
+set -e
 cd /usr/src/app || exit
-if [ ! -d "node_modules" ]; then
+touch .pkg.sha1
+OLD_SHA=$(cat .pkg.sha1)
+NEW_SHA=$(sha1sum package.json)
+if [ "$OLD_SHA" != "$NEW_SHA" ]; then
+  echo "$NEW_SHA" > .pkg.sha1
   npm install
   rm -rf node_modules/cp-translations
   ln -s /usr/src/cp-translations node_modules/cp-translations
 fi
-nodemon service.js
+npm run dev
