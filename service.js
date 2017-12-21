903daf50d6cd31b62e4f3e58c95ef91890035260
Cian Butler <butlerx@notthe.cloud>
2017-12-21 09:46:44 +0000

    log fatal erros cleaner (#380)
    * log fatal erros cleaner
    
    * string template

--- service.js
+++ service.js
@@ -1,22 +1,23 @@
-'use strict';
 process.setMaxListeners(0);
 require('events').EventEmitter.prototype._maxListeners = 100;
 
 if (process.env.NEW_RELIC_ENABLED === 'true') require('newrelic');
 
-var config = require('./config/config.js')();
-var seneca = require('seneca')(config);
-var util = require('util');
-var _ = require('lodash');
-var store = require('seneca-postgresql-store');
-var storeQuery = require('seneca-store-query');
-var dgram = require('dgram');
-var service = 'cp-dojos-service';
-var log = require('cp-logs-lib')({name: service, level: 'warn'});
-var sanitizeHtml = require('sanitize-html');
+const config = require('./config/config.js')();
+const seneca = require('seneca')(config);
+const util = require('util');
+const _ = require('lodash');
+const store = require('seneca-postgresql-store');
+const storeQuery = require('seneca-store-query');
+const dgram = require('dgram');
+const service = 'cp-dojos-service';
+const log = require('cp-logs-lib')({ name: service, level: 'warn' });
+const sanitizeHtml = require('sanitize-html');
 config.log = log.log;
 // logger creates a circular JSON
-seneca.log.info('using config', JSON.stringify(config, null, 4));
+if (process.env.NODE_ENV !== 'production') {
+  seneca.log.info('using config', JSON.stringify(config, null, 4));
+}
 
 seneca.options(config);
 seneca.options.sanitizeTextArea = {
@@ -43,17 +44,18 @@ if (process.env.MAILTRAP_ENABLED === 'true') {
 }
 seneca.use(require('./email-notifications.js'));
 seneca.use(require('seneca-kue'));
-seneca.use(require('./dojos.js'),
-  {limits: config.limits,
-    shared: config.shared,
-   'google-api': config['google-api'],
-   postgresql: config['postgresql-store'],
-   kue: config.kue,
-   logger: log.logger
- });
+seneca.use(require('./dojos.js'), {
+  limits: config.limits,
+  shared: config.shared,
+  'google-api': config['google-api'],
+  postgresql: config['postgresql-store'],
+  kue: config.kue,
+  logger: log.logger
+});
 seneca.use(require('seneca-queue'));
 seneca.use(require('cp-permissions-plugin'), {
-  config: __dirname + '/config/permissions-rules'});
+  config: `${__dirname}/config/permissions-rules`
+});
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
@@ -62,15 +64,22 @@ process.on('SIGUSR2', shutdown);
 
 function shutdown (err) {
   seneca.act({ role: 'queue', cmd: 'stop' });
-  if (err !== void 0 && err.stack !== void 0) {
-    console.error(new Date().toString() + ' FATAL: UncaughtException, please report: ' + util.inspect(err));
-    console.error(util.inspect(err.stack));
-    console.trace();
+  if (err !== undefined) {
+    const error = {
+      date: new Date().toString(),
+      msg:
+        err.stack !== undefined
+          ? `FATAL: UncaughtException, please report: ${util.inspect(err.stack)}`
+          : 'FATAL: UncaughtException, no stack trace',
+      err: util.inspect(err)
+    };
+    console.error(JSON.stringify(error));
+    process.exit(1);
   }
   process.exit(0);
 }
 
-require('./migrate-psql-db.js')(function (err) {
+require('./migrate-psql-db.js')(err => {
   if (err) {
     console.error(err);
     process.exit(-1);
@@ -78,31 +87,31 @@ require('./migrate-psql-db.js')(function (err) {
   console.log('Migrations ok');
   require('./network')(seneca);
 });
-seneca.ready(function () {
+seneca.ready(() => {
   seneca.act({ role: 'queue', cmd: 'start' });
-  var message = new Buffer(service);
+  const message = new Buffer(service);
 
-  var client = dgram.createSocket('udp4');
-  client.send(message, 0, message.length, 11404, 'localhost', function (err, bytes) {
+  const client = dgram.createSocket('udp4');
+  client.send(message, 0, message.length, 11404, 'localhost', (err, bytes) => {
     if (err) {
       console.error(err);
       process.exit(-1);
     }
     client.close();
   });
-  var escape = require('seneca-standard-query/lib/relational-util').escapeStr;
-  ['load', 'list'].forEach(function (cmd) {
-    seneca.wrap('role: entity, cmd: ' + cmd, function filterFields (args, cb) {
+  const escape = require('seneca-standard-query/lib/relational-util').escapeStr;
+  ['load', 'list'].forEach(cmd => {
+    seneca.wrap(`role: entity, cmd: ${cmd}`, function filterFields (args, cb) {
       try {
-        ['limit$', 'skip$'].forEach(function (field) {
-          if (args.q[field] && args.q[field] !== 'NULL' && !/^[0-9]+$/g.test(args.q[field] + '')) {
+        ['limit$', 'skip$'].forEach(field => {
+          if (args.q[field] && args.q[field] !== 'NULL' && !/^[0-9]+$/g.test(`${args.q[field]}`)) {
             throw new Error('Expect limit$, skip$ to be a number');
           }
         });
         if (args.q.sort$) {
           if (args.q.sort$ && typeof args.q.sort$ === 'object') {
-            var order = args.q.sort$;
-            _.each(order, function (ascdesc, column) {
+            const order = args.q.sort$;
+            _.each(order, (ascdesc, column) => {
               if (!/^[a-zA-Z0-9_]+$/g.test(column)) {
                 throw new Error('Unexpect characters in sort$');
               }
@@ -112,8 +121,8 @@ seneca.ready(function () {
           }
         }
         if (args.q.fields$) {
-          args.q.fields$.forEach(function (field, index) {
-            args.q.fields$[index] = '\"' + escape(field) + '\"';
+          args.q.fields$.forEach((field, index) => {
+            args.q.fields$[index] = `"${escape(field)}"`;
           });
         }
         this.prior(args, cb);
