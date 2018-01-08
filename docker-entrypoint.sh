1b9691f2eb12955f92a447fd3d2ba8fdedb24acd
Daniel Brierton <daniel@brierton.me>
2018-01-08 09:58:31 +0000

    replace npm w/ yarn due to failure to install packages on alpine (#390)
    * replace npm w/ yarn due to failure to install packages on alpine
    
    * Update circleci conf for npm -> yarn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env sh
 set -e
 cd /usr/src/app || exit
-touch .pkg.sha1
-OLD_SHA=$(cat .pkg.sha1)
-NEW_SHA=$(sha1sum package.json)
+touch .yarn.sha1
+OLD_SHA=$(cat .yarn.sha1)
+NEW_SHA=$(sha1sum yarn.lock)
 if [ "$OLD_SHA" != "$NEW_SHA" ]; then
-  echo "$NEW_SHA" > .pkg.sha1
-  npm install
+  echo "$NEW_SHA" > .yarn.sha1
+  yarn
   rm -rf node_modules/cp-translations
   ln -s /usr/src/cp-translations node_modules/cp-translations
 fi
-npm run dev
+yarn dev
