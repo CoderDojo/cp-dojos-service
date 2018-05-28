4d485917b3ffb596550e80fc33cf4277faddea15
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-05-28 10:39:16 +0100

    Remove mailtrap configuration

--- config/config.js
+++ config/config.js
@@ -50,20 +50,6 @@ module.exports = function (options) {
         headers: {'X-SMTPAPI': '{"category": ["cp-dojos-service"]}'}
       }
     },
-    mailtrap: {
-      folder: path.resolve(CpTranslations.getEmailTemplatePath()),
-      mail: {
-        from: 'no-reply@coderdojo.com'
-      },
-      config: {
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS
-        }
-      }
-    },
     maildev: {
       folder: path.resolve(CpTranslations.getEmailTemplatePath()),
       mail: {
