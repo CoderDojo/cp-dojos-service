df42384c35dc594df7f85a98cde4553e952675c4
Cian Butler <butlerx@notthe.cloud>
2018-06-22 16:41:18 +0100

    use kue to avoid email dojo crashing (#385)
    * use kue to avoid email dojo crashing
    
    * Fix missing dependencies

--- dojos.js
+++ dojos.js
@@ -56,6 +56,7 @@ var cmd_can_award_badge = require('./lib/perm/can-award-badge');
 
 var cmd_get_dojo_stats = require('./lib/dojos/stats');
 var cmd_search_join_requests = require('./lib/dojos/search-join-requests');
+var cmd_notify_all_members = require('./lib/dojos/users/notifyAll');
 
 var cmd_backfill_champions = require('./lib/backfill-champions');
 // Utils
@@ -209,7 +210,7 @@ module.exports = function (options) {
   seneca.add({role: plugin, cmd: 'migrate-v1-leads'}, require('./lib/migrate-old-lead'));
 
   if (options.kue && options.kue.start) {
-    var kues = ['batch-poller', 'sms-poll', 'email-poll'];
+    var kues = ['batch-poller', 'sms-poll', 'email-poll', 'email-user'];
 
     seneca.act({role: 'kue-queue', cmd: 'start', config: options.kue}, function (err, queue) {
       if (!err) {
@@ -1441,122 +1442,6 @@ module.exports = function (options) {
         return done(null, {email: dojoEmail});
       }
     });
-  }
-
-  function cmd_notify_all_members (args, done) {
-    //  TODO:40 enqueue this process
-    var seneca = this;
-    var dojoId = args.data.dojoId;
-    var eventId = args.data.eventId;
-    var zenHostname = process.env.HOSTNAME || '127.0.0.1:8000';
-    var emailSubject = args.data.emailSubject;
-
-    async.waterfall([
-      getDojoUsers,
-      checkEmail,
-      getEvent,
-      getDojo,
-      sendEmails
-    ], done);
-
-    function getDojoUsers (done) {
-      var query = {dojoId: dojoId};
-
-      seneca.act({role: plugin, cmd: 'load_dojo_users', query: query}, function (err, response) {
-        if (err) return done(err);
-        done(null, response.response);
-      });
-    }
-
-    function checkEmail (users, done) {
-      async.map(users, function (user, callback) {
-        if (_.isEmpty(user.email)) {
-          seneca.act({role: 'cd-profiles', cmd: 'load_parents_for_user', userId: user.id, user: args.user}, function (err, parents) {
-            if (err) return seneca.log.warn('No parent found for', user.id);
-            //  TODO:80 handle multiple parents
-            user.parent = parents[0];
-
-            //  excluse this child if the parent email is already in the list, to avoid multiple emails
-            if (_.some(users, {email: user.parent.email})) {
-              user = void 0;
-            }
-            callback(null, user);
-          });
-        } else {
-          callback(null, user);
-        }
-      },
-      function (err, users) {
-        if (err) done(err);
-        done(null, _.pull(users, void 0));
-      });
-    }
-
-    function getEvent (users, done) {
-      seneca.act({role: 'cd-events', cmd: 'getEvent', id: eventId}, function (err, event) {
-        if (err) return done(err);
-        done(null, users, event);
-      });
-    }
-
-    function getDojo (users, event, done) {
-      seneca.act({role: plugin, cmd: 'load', id: dojoId}, function (err, dojo) {
-        if (err) return done(err);
-        done(null, users, event, dojo);
-      });
-    }
-
-    function sendEmails (users, event, dojo, done) {
-      if (users) {
-        var content = {
-          link: protocol + '://' + zenHostname + '/dashboard/dojo/' + dojo.urlSlug,
-          dojo: {
-            name: dojo.name,
-            email: dojo.email
-          },
-          event: {
-            name: event.name
-          },
-          year: moment(new Date()).format('YYYY')
-        };
-
-        var code = '';
-        var baseCode = '';
-        if (event.type === 'recurring') {
-          baseCode = 'notify-all-members-recurring-';
-        } else {
-          baseCode = 'notify-all-members-oneoff-';
-          var startDateUtcOffset = moment(_.head(event.dates).startTime).utcOffset();
-          var endDateUtcOffset = moment(_.head(event.dates).endTime).utcOffset();
-
-          var startDate = moment.utc(_.head(event.dates).startTime).subtract(startDateUtcOffset, 'minutes').toDate();
-          var endDate = moment.utc(_.head(event.dates).endTime).subtract(endDateUtcOffset, 'minutes').toDate();
-
-          content.event.date = moment(startDate).format('Do MMMM YY') + ', ' +
-            moment(startDate).format('HH:mm') + ' - ' +
-            moment(endDate).format('HH:mm');
-        }
-        var locality = args.locality || 'en_US';
-
-        _.forEach(users, function (user) {
-          content.dojoMember = user.name;
-          var email = user.email;
-          code = baseCode;
-          if (!_.isEmpty(user.parent) && !_.isEmpty(user.parent.email)) {
-            email = user.parent.email;
-            code = 'parents-' + baseCode;
-            content.childrenName = user.name;
-            content.dojoMember = user.parent.name;
-          }
-          if (!_.isEmpty(email)) {
-            var payload = {replyTo: dojo.email, from: '"' + dojo.name + '" <' + dojo.email + '>', to: email,
-              code: code, locality: locality, content: content, subject: emailSubject, subjectVariables: [dojo.name]};
-            seneca.act({role: plugin, cmd: 'send_email', payload: _.cloneDeep(payload)});
-          }
-        });
-        done();
-      }
-    }
   }
 
   // from countries service
